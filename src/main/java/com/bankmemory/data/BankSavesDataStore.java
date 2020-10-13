9fdd5bbab5aa3b786c85e530c5a089798a03390f
Samuel <6756161+Lazyfaith@users.noreply.github.com>
2020-10-13 19:05:09 +0100

    Make BankSavesDataStore thread safe and thread smart
    Since in coming changes it will start to be interacted with from
    multiple threads. By moving the JSON serialisation to the background
    thread it stops the game/the UI lagging due to the time the
    serialisation takes.

--- src/main/java/com/bankmemory/data/BankSavesDataStore.java
+++ src/main/java/com/bankmemory/data/BankSavesDataStore.java
@@ -11,6 +11,9 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.config.ConfigManager;
 
@@ -19,15 +22,20 @@ public class BankSavesDataStore {
     private static final String PLUGIN_BASE_GROUP = "bankMemory";
     private static final String CURRENT_LIST_KEY = "currentList";
 
+    private final Object dataLock = new Object();
     private final ConfigManager configManager;
     private final ItemDataParser itemDataParser;
     private final List<BankSave> currentBankList;
+    private final BlockingQueue<ConfigWrite> configWritesQueue = new LinkedBlockingQueue<>();
 
     @Inject
     private BankSavesDataStore(ConfigManager configManager, ItemDataParser itemDataParser) {
         this.configManager = configManager;
         this.itemDataParser = itemDataParser;
         currentBankList = loadCurrentBankList();
+        Thread configWriter = new Thread(new ConfigWriter(), "Bank Memory config writer");
+        configWriter.setDaemon(true);
+        configWriter.start();
     }
 
     private List<BankSave> loadCurrentBankList() {
@@ -60,12 +68,20 @@ public class BankSavesDataStore {
         if (Strings.isNullOrEmpty(login)) {
             return Optional.empty();
         }
-        return currentBankList.stream()
-                .filter(s -> s.getUserName().equalsIgnoreCase(login))
-                .findAny();
+        synchronized (dataLock) {
+            return currentBankList.stream()
+                    .filter(s -> s.getUserName().equalsIgnoreCase(login))
+                    .findAny();
+        }
     }
 
     public void saveAsCurrentBank(BankSave newSave) {
+        synchronized (dataLock) {
+            saveAsCurrentBankImpl(newSave);
+        }
+    }
+
+    private void saveAsCurrentBankImpl(BankSave newSave) {
         // Check if there is a current bank for existing login and remove it
         currentBankList.stream()
                 .filter(s -> s.getUserName().equalsIgnoreCase(newSave.getUserName()))
@@ -74,7 +90,44 @@ public class BankSavesDataStore {
 
         // Save new current bank at top of list
         currentBankList.add(0, newSave);
-        Gson gson = buildGson();
-        configManager.setConfiguration(PLUGIN_BASE_GROUP, CURRENT_LIST_KEY, gson.toJson(currentBankList));
+        ConfigWrite configWrite = new ConfigWrite(
+                PLUGIN_BASE_GROUP, CURRENT_LIST_KEY, new ArrayList<>(currentBankList));
+        try {
+            log.debug("Scheduling write for {}.{}", configWrite.configGroup, configWrite.configKey);
+            configWritesQueue.put(configWrite);
+        } catch (InterruptedException ex) {
+            log.error("Unexpected interrupt whilst schedule config write. Data not being written", ex);
+        }
+    }
+
+    @AllArgsConstructor
+    private static class ConfigWrite {
+        final String configGroup;
+        final String configKey;
+        final Object data;
+    }
+
+    // NB: technically there's a race condition because it's possible that the client could shut down before the call
+    // to the ConfigManager is made. However, us trying to react to a client shutdown by writing all the remaining
+    // writes won't work since the ConfigManager is the first thing to react to a client shutdown and won't perform any
+    // new writes once it has.
+    // Not very likely to happen anyway since it basically requires user to do something in game (to trigger plugin) and
+    // then super quickly (like <10ms) close the client.
+    private class ConfigWriter implements Runnable {
+        @Override
+        public void run() {
+            Gson gson = buildGson();
+            while (!Thread.interrupted()) {
+                ConfigWrite write;
+                try {
+                    write = configWritesQueue.take();
+                    log.debug("Got write for {}.{}", write.configGroup, write.configKey);
+                } catch (InterruptedException ex) {
+                    log.warn("ConfigWriter thread interrupted", ex);
+                    break;
+                }
+                configManager.setConfiguration(write.configGroup, write.configKey, gson.toJson(write.data));
+            }
+        }
     }
 }
