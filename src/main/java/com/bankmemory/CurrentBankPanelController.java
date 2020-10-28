Lazyfaith/runelite-bank-memory-plugin

commit b73626ae19d7ddd1fa26b20e4cd84df2aa3a4bd6

Differentiate bank saves for special mode worlds
League worlds, tournament worlds, deadman mode worlds, and deadman mode
tournament worlds are all worlds where the same account (same login,
same username) will have a completely different bank account to a
"normal" world.

Not 100% happy with the UI changes/additions needed for this, but
they'll do. And hopefully I'll think of something better in the future.

src/main/java/com/bankmemory/CurrentBankPanelController.java
@@ -2,6 +2,7 @@ package com.bankmemory;
 
 import com.bankmemory.data.BankItem;
 import com.bankmemory.data.BankSave;
+import com.bankmemory.data.BankWorldType;
 import com.bankmemory.data.PluginDataStore;
 import java.util.ArrayList;
 import java.util.List;
@@ -47,12 +48,16 @@ public class CurrentBankPanelController {
     }
 
     private void updateDisplayForCurrentAccount() {
-        Optional<BankSave> existingSave = dataStore.getDataForCurrentBank(client.getUsername());
+        BankWorldType worldType = BankWorldType.forWorld(client.getWorldType());
+        Optional<BankSave> existingSave = dataStore.getDataForCurrentBank(worldType, client.getUsername());
         if (existingSave.isPresent()) {
             BankSave save = existingSave.get();
-            if (latestDisplayedData != null
-                    && !latestDisplayedData.getUserName().equalsIgnoreCase(save.getUserName())) {
-                SwingUtilities.invokeLater(panel::reset);
+            if (latestDisplayedData != null) {
+                boolean userNameDifferent = latestDisplayedData.getUserName().equalsIgnoreCase(save.getUserName());
+                boolean worldTypeDifferent = latestDisplayedData.getWorldType() != save.getWorldType();
+                if (userNameDifferent || worldTypeDifferent) {
+                    SwingUtilities.invokeLater(panel::reset);
+                }
             }
             handleBankSave(existingSave.get());
         } else {

src/main/java/com/bankmemory/BankDiffPanel.java
@@ -1,5 +1,8 @@
 package com.bankmemory;
 
+import com.bankmemory.data.BankWorldType;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -19,6 +22,7 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.components.ComboBoxListRenderer;
 import net.runelite.client.util.ImageUtil;
 import net.runelite.client.util.SwingUtil;
@@ -32,8 +36,8 @@ public class BankDiffPanel extends JPanel {
 
     private static final Icon REVERSE_ICON;
     private static final Icon REVERSE_ICON_HOVER;
-    private static final String CURRENT_BANKS = "Current banks";
-    private static final String SNAPSHOT_BANKS = "Snapshots";
+    private static final String CURRENT_BANKS = "- Current banks -";
+    private static final String SNAPSHOT_BANKS = "- Snapshots -";
 
     private final BankViewPanel itemsList = new BankViewPanel();
     private final OptionsListModel beforeOptionsModel = new OptionsListModel();
@@ -114,11 +118,11 @@ public class BankDiffPanel extends JPanel {
         List<Object> options = new ArrayList<>();
         if (!currentBanks.isEmpty()) {
             options.add(CURRENT_BANKS);
-            options.addAll(currentBanks);
+            options.addAll(listWithWorldSeparators(currentBanks));
         }
         if (!snapshotBanks.isEmpty()) {
             options.add(SNAPSHOT_BANKS);
-            options.addAll(snapshotBanks);
+            options.addAll(listWithWorldSeparators(snapshotBanks));
         }
         disableSelectionListener = true;
         beforeOptionsModel.removeAllElements();
@@ -128,6 +132,21 @@ public class BankDiffPanel extends JPanel {
             afterOptionsModel.addElement(o);
         });
         disableSelectionListener = false;
+    }
+
+    private List<Object> listWithWorldSeparators(List<BankDiffListOption> options) {
+        ListMultimap<BankWorldType, BankDiffListOption> optionsByWorldType = ArrayListMultimap.create();
+        options.forEach(o -> optionsByWorldType.put(o.getSave().getWorldType(), o));
+        List<Object> resultList = new ArrayList<>();
+        for (BankWorldType type : BankWorldType.values()) {
+            if (optionsByWorldType.containsKey(type)) {
+                if (type != BankWorldType.DEFAULT) {
+                    resultList.add(type.getDisplayString());
+                }
+                resultList.addAll(optionsByWorldType.get(type));
+            }
+        }
+        return resultList;
     }
 
     void setSelections(BankDiffListOption before, BankDiffListOption after) {
@@ -181,13 +200,17 @@ public class BankDiffPanel extends JPanel {
 
             Component comp = wrapped.getListCellRendererComponent(list, valToRender, index, isSelected, cellHasFocus);
 
-            int fontStyle = Font.PLAIN;
+            Font font = FontManager.getRunescapeFont();
             Color fgColour = comp.getForeground();
             if (!(value instanceof BankDiffListOption)) {
-                fontStyle = Font.BOLD;
+                if (value == CURRENT_BANKS || value == SNAPSHOT_BANKS) {
+                    font = FontManager.getRunescapeBoldFont();
+                } else {
+                    font = FontManager.getRunescapeSmallFont().deriveFont(Font.ITALIC);
+                }
                 fgColour = ColorScheme.BRAND_ORANGE;
             }
-            comp.setFont(comp.getFont().deriveFont(fontStyle));
+            comp.setFont(font);
             comp.setForeground(fgColour);
             return comp;
         }

src/main/java/com/bankmemory/BankMemoryPlugin.java
@@ -1,6 +1,7 @@
 package com.bankmemory;
 
 import com.bankmemory.data.BankSave;
+import com.bankmemory.data.BankWorldType;
 import com.bankmemory.data.PluginDataStore;
 import com.bankmemory.util.Constants;
 import java.awt.image.BufferedImage;
@@ -106,7 +107,9 @@ public class BankMemoryPlugin extends Plugin {
         if (event.getContainerId() != InventoryID.BANK.getId()) {
             return;
         }
+        BankWorldType worldType = BankWorldType.forWorld(client.getWorldType());
         ItemContainer bank = event.getItemContainer();
-        currentBankPanelController.handleBankSave(BankSave.fromCurrentBank(client.getUsername(), bank, itemManager));
+        currentBankPanelController.handleBankSave(
+                BankSave.fromCurrentBank(worldType, client.getUsername(), bank, itemManager));
     }
 }

src/main/java/com/bankmemory/BanksListEntry.java
@@ -1,5 +1,6 @@
 package com.bankmemory;
 
+import com.bankmemory.data.BankWorldType;
 import javax.swing.ImageIcon;
 import lombok.Value;
 
@@ -7,6 +8,7 @@ import lombok.Value;
 public class BanksListEntry {
     long saveId;
     ImageIcon icon;
+    BankWorldType worldType;
     String saveName;
     String accountDisplayName;
     String dateTime;

src/main/java/com/bankmemory/BanksListPanel.java
@@ -1,10 +1,10 @@
 package com.bankmemory;
 
+import com.bankmemory.data.BankWorldType;
 import com.bankmemory.util.Constants;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.MouseInfo;
@@ -23,6 +23,7 @@ import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.components.PluginErrorPanel;
 
 
@@ -160,9 +161,13 @@ public class BanksListPanel extends JPanel {
             add(new JLabel(entry.getSaveName()), c);
 
             c.gridy = 1;
-            JLabel subText = new JLabel(entry.getAccountDisplayName());
-            subText.setFont(subText.getFont().deriveFont(Font.ITALIC));
-            add(subText, c);
+            String subText = entry.getAccountDisplayName();
+            if (entry.getWorldType() != BankWorldType.DEFAULT) {
+                subText += " (" + entry.getWorldType().getDisplayString() + ")";
+            }
+            JLabel subLabel = new JLabel(subText);
+            subLabel.setFont(FontManager.getRunescapeSmallFont());
+            add(subLabel, c);
 
             addMouseListener(mouseListener);
         }

src/main/java/com/bankmemory/SavedBanksPanelController.java
@@ -56,12 +56,12 @@ public class SavedBanksPanelController {
         for (BankSave save : dataStore.getCurrentBanksList()) {
             String displayName = nameMapper.map(save.getUserName());
             saves.add(new BanksListEntry(
-                    save.getId(), casketIcon, "Current bank", displayName, save.getDateTimeString()));
+                    save.getId(), casketIcon, save.getWorldType(), "Current bank", displayName, save.getDateTimeString()));
         }
         for (BankSave save : dataStore.getSnapshotBanksList()) {
             String displayName = nameMapper.map(save.getUserName());
             saves.add(new BanksListEntry(
-                    save.getId(), notedCasketIcon, save.getSaveName(), displayName, save.getDateTimeString()));
+                    save.getId(), notedCasketIcon, save.getWorldType(), save.getSaveName(), displayName, save.getDateTimeString()));
         }
 
         Runnable updateList = () -> topPanel.updateBanksList(saves);

src/main/java/com/bankmemory/data/BankSave.java
@@ -20,6 +20,7 @@ public class BankSave {
     private static final AtomicInteger idIncrementer = new AtomicInteger();
 
     long id;
+    BankWorldType worldType;
     String dateTimeString;
     String userName;
     @Nullable String saveName;
@@ -27,18 +28,29 @@ public class BankSave {
 
     @VisibleForTesting
     public BankSave(
+            BankWorldType worldType,
             String userName,
             @Nullable String saveName,
             String dateTimeString,
             ImmutableList<BankItem> itemData) {
         id = ID_BASE + idIncrementer.incrementAndGet();
+        this.worldType = worldType;
         this.userName = userName;
         this.saveName = saveName;
         this.dateTimeString = dateTimeString;
         this.itemData = itemData;
     }
 
-    public static BankSave fromCurrentBank(String userName, ItemContainer bank, ItemManager itemManager) {
+    // TODO: when I create a proper data upgrader/transitioner thing, remove this and put relevant logic there
+    public BankWorldType getWorldType() {
+        return worldType == null ? BankWorldType.DEFAULT : worldType;
+    }
+
+    public static BankSave fromCurrentBank(
+            BankWorldType worldType,
+            String userName,
+            ItemContainer bank,
+            ItemManager itemManager) {
         Objects.requireNonNull(bank);
         net.runelite.api.Item[] contents = bank.getItems();
         ImmutableList.Builder<BankItem> itemData = ImmutableList.builder();
@@ -54,12 +66,17 @@ public class BankSave {
             itemData.add(new BankItem(canonId, item.getQuantity()));
         }
         String timeString = DATE_FORMATTER.format(ZonedDateTime.now());
-        return new BankSave(userName, null, timeString, itemData.build());
+        return new BankSave(worldType, userName, null, timeString, itemData.build());
     }
 
     public static BankSave snapshotFromExistingBank(String newName, BankSave existingBank) {
         Objects.requireNonNull(newName);
-        return new BankSave(existingBank.userName, newName, existingBank.dateTimeString, existingBank.itemData);
+        return new BankSave(
+                existingBank.worldType,
+                existingBank.userName,
+                newName,
+                existingBank.dateTimeString,
+                existingBank.itemData);
     }
 
     @Override

src/main/java/com/bankmemory/data/BankWorldType.java
@@ -1,0 +1,35 @@
+package com.bankmemory.data;
+
+import java.util.EnumSet;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.runelite.api.WorldType;
+
+@AllArgsConstructor
+@Getter
+public enum BankWorldType {
+    // NB: changing these name will break automatic JSON deserialisation of existing saves
+    DEFAULT(""),
+    LEAGUE("League"),
+    TOURNAMENT("Tournament"),
+    DEADMAN("DMM"),
+    DEADMAN_TOURNAMENT("DMM Tournament");
+
+    String displayString;
+
+    public static BankWorldType forWorld(EnumSet<WorldType> worldsTypes) {
+        if (worldsTypes.contains(WorldType.LEAGUE)) {
+            return BankWorldType.LEAGUE;
+        }
+        if (worldsTypes.contains(WorldType.TOURNAMENT)) {
+            return BankWorldType.TOURNAMENT;
+        }
+        if (worldsTypes.contains(WorldType.DEADMAN_TOURNAMENT)) {
+            return BankWorldType.DEADMAN_TOURNAMENT;
+        }
+        if (worldsTypes.contains(WorldType.DEADMAN)) {
+            return BankWorldType.DEADMAN;
+        }
+        return BankWorldType.DEFAULT;
+    }
+}

commit 756e565fd900acbe19b1154e53b2bce2fe70d3f6

Fix bug where current bank panel could be accidentally left reset
Bug was introduced with commit:

	b73626ae19d7ddd1fa26b20e4cd84df2aa3a4bd6
	Differentiate bank saves for special mode worlds

The issue was with CurrentBankPanelController:56. The result of the
 #equalsIgnoreCase call should have been negated but was not. Because of
this, the items list could be reset even if there was nothing new to
display. And when there's nothing new to display, the panel was left
blank until the items list would be updated.

This changes bring the call to #reset closer to where the items lists is
updated and links the logic a bit more, hopefully making the code
resistant to such a bug in the future (at least here).

This is probably a sign that I should start writing more tests...

src/main/java/com/bankmemory/CurrentBankPanelController.java
@@ -51,14 +51,6 @@ public class CurrentBankPanelController {
         BankWorldType worldType = BankWorldType.forWorld(client.getWorldType());
         Optional<BankSave> existingSave = dataStore.getDataForCurrentBank(worldType, client.getUsername());
         if (existingSave.isPresent()) {
-            BankSave save = existingSave.get();
-            if (latestDisplayedData != null) {
-                boolean userNameDifferent = latestDisplayedData.getUserName().equalsIgnoreCase(save.getUserName());
-                boolean worldTypeDifferent = latestDisplayedData.getWorldType() != save.getWorldType();
-                if (userNameDifferent || worldTypeDifferent) {
-                    SwingUtilities.invokeLater(panel::reset);
-                }
-            }
             handleBankSave(existingSave.get());
         } else {
             latestDisplayedData = null;
@@ -71,9 +63,10 @@ public class CurrentBankPanelController {
 
         dataStore.saveAsCurrentBank(newSave);
 
-        boolean isDataNew = isItemDataNew(newSave);
+        boolean shouldReset = isBankIdentityDifferentToLastDisplayed(newSave);
+        boolean shouldUpdateItemsDisplay = shouldReset || isItemDataNew(newSave);
         List<ItemListEntry> items = new ArrayList<>();
-        if (isDataNew) {
+        if (shouldUpdateItemsDisplay) {
             // Get all the data we need for the UI on this thread (the game thread)
             // Doing it on the EDT seems to cause random crashes & NPEs
             for (BankItem i : newSave.getItemData()) {
@@ -85,12 +78,24 @@ public class CurrentBankPanelController {
             }
         }
         SwingUtilities.invokeLater(() -> {
+            if (shouldReset) {
+                panel.reset();
+            }
             panel.updateTimeDisplay(newSave.getDateTimeString());
-            if (isDataNew) {
+            if (shouldUpdateItemsDisplay) {
                 panel.displayItemListings(items, true);
             }
         });
         latestDisplayedData = newSave;
+    }
+
+    private boolean isBankIdentityDifferentToLastDisplayed(BankSave newSave) {
+        if (latestDisplayedData == null) {
+            return true;
+        }
+        boolean userNamesSame = latestDisplayedData.getUserName().equalsIgnoreCase(newSave.getUserName());
+        boolean worldTypesSame = latestDisplayedData.getWorldType() == newSave.getWorldType();
+        return !userNamesSame || !worldTypesSame;
     }
 
     private boolean isItemDataNew(BankSave newSave) {